ce7f593e9ac61a14c479c8be525c76d6dd833d58
StBanas <pio.banasik@gmail.com>
2021-12-07 11:58:51 +0100

    Add files via upload

---
+++ Donkey.java
@@ -0,0 +1,17 @@
+
+public class Donkey {
+
+    public String name;
+    private Baloons weight;
+
+    public Donkey(String name, double weight) {
+        this.name = name;
+        this.weight = new Baloons();
+    }
+
+    public void addBallonChecker(double baloon) {
+        if (baloon > 0.005 && baloon < 0.007) {
+            this.addBallonChecker(baloon);
+        }
+    }
+}
---
+++ Up_load.java
@@ -0,0 +1,7 @@
+public class Up_load {
+    boolean isSufficient;
+
+    public boolean isSufficient() {
+        return (true);
+    }
+}
